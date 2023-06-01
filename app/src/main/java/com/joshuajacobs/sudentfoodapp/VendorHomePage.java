1449f34dff42151f7b95fc8178fffdc065b9cc92
MZainGamildien <102467824+MZainGamildien@users.noreply.github.com>
2023-06-01 17:30:49 +0200

    Login,homepage of vendor

---
+++ app/src/main/java/com/joshuajacobs/sudentfoodapp/VendorHomePage.java
@@ -0,0 +1,14 @@
+package com.joshuajacobs.sudentfoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VendorHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_home_page);
+    }
+}
--- app/src/main/java/com/joshuajacobs/sudentfoodapp/VendorOrCustomer.java
+++ app/src/main/java/com/joshuajacobs/sudentfoodapp/VendorOrCustomer.java
@@ -15,11 +15,18 @@ public class VendorOrCustomer extends AppCompatActivity {
         setContentView(R.layout.activity_vendor_or_customer);
 
         Button vendorBtn=(Button)findViewById(R.id.signup_button);
+        Button custBtn= (Button)findViewById((R.id.signup_buttonCustomer)) ;
         vendorBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(VendorOrCustomer.this,RegisterVendor.class));
 
+            }
+        });
+        custBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(VendorOrCustomer.this,ClientSignUpActivity.class));
             }
         });
     }

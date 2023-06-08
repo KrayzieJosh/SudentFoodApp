db1a476ea76e66e8981c060a7142c432187c1624
Ferdouz <ferdouzallie1@gmail.com>
2023-06-08 22:01:07 +0200

    Changes made to the activity_order.xml and OrderActivity

--- app/src/main/java/com/joshuajacobs/sudentfoodapp/OrderActivity.java
+++ app/src/main/java/com/joshuajacobs/sudentfoodapp/OrderActivity.java
@@ -1,28 +1,51 @@
 package com.joshuajacobs.sudentfoodapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.joshuajacobs.sudentfoodapp.Payments;
-import com.joshuajacobs.sudentfoodapp.R;
 
-    public class OrderActivity extends AppCompatActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_order);
+public class OrderActivity extends AppCompatActivity {
 
-            Button proceedToPay = findViewById(R.id.proceedPay);
-            proceedToPay.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(OrderActivity.this , Payments.class));
-                }
-                //fix emulator issue
+    private Button buttonRemoveItem;
+    private Button buttonProceedToPayments;
 
-            });
-        }}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        buttonRemoveItem = findViewById(R.id.buttonRemoveItem);
+        buttonProceedToPayments = findViewById(R.id.buttonProceedToPayments);
+
+        buttonRemoveItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform the functionality for removing an item
+                removeItem();
+            }
+        });
+
+        buttonProceedToPayments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform the functionality for proceeding to payments
+                proceedToPayments();
+            }
+        });
+    }
+
+    private void removeItem() {
+        // Logic to remove an item from the order
+        Toast.makeText(this, "Item removed from the order", Toast.LENGTH_SHORT).show();
+    }
+
+    private void proceedToPayments() {
+        // Logic to proceed to the payments screen
+        Toast.makeText(this, "Proceeding to payments", Toast.LENGTH_SHORT).show();
+    }
+}
 
 
